ca7a24691f3bf571d9a5101f2de414cc8a622a8a
f776655321 <f77665532166@gmail.com>
2023-03-06 00:18:34 +0800

    remote-week3: axios change

--- remote-assigment3/app/src/App.js
+++ remote-assigment3/app/src/App.js
@@ -33,7 +33,7 @@ function App() {
     <div className = "SignupWrapper">
       <input placeholder = "Name" value={name} onChange={(e)=>{setName(e.target.value)}}/>
       <input placeholder = "Email" value={email} onChange={(e)=>{setEmail(e.target.value)}}/>
-      <input placeholder = "Password" value={password} onChange={(e)=>{setPassword(e.target.value)}}/>
+      <input type = "password" placeholder = "Password" value={password} onChange={(e)=>{setPassword(e.target.value)}}/>
       <button onClick={()=>{handlesubmit()}}>submit</button>
       <p>{result}</p>
     </div>
--- remote-assigment3/app/src/axios.js
+++ remote-assigment3/app/src/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 const instance = axios.create({
-    baseURL: 'http://35.77.148.148:3000'
+    baseURL: 'http://35.77.148.148/api'
 })
 export default instance
